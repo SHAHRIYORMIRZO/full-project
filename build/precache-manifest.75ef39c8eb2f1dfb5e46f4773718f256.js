e59d792de6c0c4e0738cfd221b2e88fac8ad70af
SHAHRIYORMIRZO <sakbaraliyev8@gmail.com>
2022-05-29 10:54:46 +0500

    backend firebase

---
+++ build/precache-manifest.75ef39c8eb2f1dfb5e46f4773718f256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4d27f404ad0a3cb86f897b4ca1d882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ca2e0d6b8cc5c394ff",
+    "url": "/static/css/main.bc140442.chunk.css"
+  },
+  {
+    "revision": "f0c76ba57cd4efe4fb2e",
+    "url": "/static/js/2.c2ec4765.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.c2ec4765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ca2e0d6b8cc5c394ff",
+    "url": "/static/js/main.29d290d5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.29d290d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
